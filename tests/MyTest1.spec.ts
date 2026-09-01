d718a76033b62e1226317ec2c289f96f769f99ef
Rahul Barik <7r.barik@gmail.com>
2026-09-01 19:31:15 +0530

    Added some notes for better understanding

--- tests/MyTest1.spec.ts
+++ tests/MyTest1.spec.ts
@@ -1,5 +1,7 @@
-import {test,expect} from "@playwright/test";
+import { test, expect } from "@playwright/test";
 //Import the test and expect functions from Playwright's testing library so that we can write and verify tests.
+//test is used to define a test case.
+//expect is used for assertions.
 
 //syntax of the test method
 
@@ -9,14 +11,31 @@ import {test,expect} from "@playwright/test";
 //     //step3
 // })
 
-// fixture - is a global varibale provided by playwright (like : page, browser)
+
+/* 
+🔗 Relationship Between async, await, and Promise
+
+A simple way to remember it is:
+
+       ASYNC FUNCTION
+             ↓
+      allows us to use
+             ↓
+           AWAIT
+             ↓
+       waits for a
+             ↓
+          PROMISE 
+          
+*/
+// Fixture: A fixture is a pre-configured resource that Playwright Test automatically provides to a test. Examples include page, browser, context, and request. We access a fixture through the test function's parameters, commonly using object destructuring: async ({ page }) => {}.
 // why page fixture - bcz we can do anythng in a page like we can launch the browser, we can add validaion  
 
-test ("Verify the page title", async ({page})=>{
+test("Verify the page title", async ({ page }) => {
     //when we use await , you must specify async before arrow function
-    //promise means - every statement return a promise like Resolved or rejected so we need o add await
+    //promise means - certain asynchronous operations return Promises like Resolved or rejected so we need o add await
     await page.goto("https://www.youtube.com/");
     let mytitle: string = await page.title();
-    console.log("Title of this page : ",mytitle);
+    console.log("Title of this page : ", mytitle);
     await expect(page).toHaveTitle("YouTube")
 })
