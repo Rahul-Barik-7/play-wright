725f5883ed8e47f318aa1dd2268f75074f336053
Rahul <7r.barik@gmail.com>
2026-09-07 01:27:32 +0530

    Added some notes

--- tests/MyTest2.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from "@playwright/test"
-
-test("Verify url of the page", async ({ page }) => {
-    await page.goto("https://testautomationpractice.blogspot.com/")
-    let myUrl: string = await page.url();
-    console.log("Url: ", myUrl);
-    await expect(page).toHaveURL(/testautomationpractice/);
-})
